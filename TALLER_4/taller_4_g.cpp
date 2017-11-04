6279dd4b653d32e0baafd25798124463a2d90b88
jorgeparra14 <30671014+jorgeparra14@users.noreply.github.com>
2017-11-03 23:49:19 -0500

    Add files via upload

---
+++ TALLER_4/taller_4_g.cpp
@@ -0,0 +1,164 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <cstring>
+
+
+struct cancion{
+	
+	char artista[40];
+	char titulo [50];
+	int duracion;
+	int tamanioKb;
+	
+};
+
+void menu();
+void ingresaDatos(cancion[],int & );
+void mostrarDatos(cancion[],int );
+void muestraTitulos(cancion[],int);
+void buscarCancion (cancion[], int);
+
+int main() {
+	
+	menu();
+	
+	return 0;
+}
+
+void ingresaDatos(cancion tema[], int &longitud){
+	
+	int i;
+	longitud=0;
+	printf ("\ningrese la cantidad de canciones:  ");
+	scanf ("%d", &longitud);
+	
+	printf ("\ningrese los datos de la cancion:\n");
+	
+	for (i=0; i<longitud; i++){
+		
+		printf ("\ncancion N° %d:\n",i+1);
+		printf ("\nartista: ");
+		fflush(stdin);
+		fgets(tema[i].artista,40,stdin);
+		
+		printf ("\ntitulo: ");
+		fflush(stdin);
+		fgets(tema[i].titulo,50,stdin);
+		
+		printf ("\nduracion en segundos: ");
+		scanf ("%d", &tema[i].duracion);
+		
+		printf ("\ntamaño en kiloBytes: ");
+		scanf ("%d", &tema[i].tamanioKb);	
+	}
+
+}
+
+
+void mostrarDatos(cancion tema[], int longitud){
+	
+	int i=0;
+	
+	for (i=0; i<longitud; i++){
+		printf ("\n\nla cancion N° %d es:\n", i+1);
+		printf ("\n\n artista: %s titulo: %s duracion: %d segundos \n tamaño: %d KB \n formato mp3",tema[i].artista,tema[i].titulo,tema[i].duracion, tema[i].tamanioKb );		
+		
+	}
+	
+	
+}
+	
+	
+
+
+void muestraTitulos(cancion tema[],int longitud){
+	
+	int i;
+	printf ("\nlistado de titulos:\n");
+	for (i=0; i<longitud; i++){
+		
+		printf ("titulo N° %d: %s",i+1 ,tema[i].titulo );
+	}
+}
+
+void buscarCancion (cancion tema[], int longitud){
+	int i;
+	char comparacion[40];
+	char nombre[40];
+	
+	
+	printf ("ingrese titulo o nombre del artista a buscar: ");
+	fflush(stdin);
+	fgets(nombre,40,stdin);
+	
+	for (i=0; i<longitud; i++){
+		
+		comparacion=tema[i].artista;
+		
+		if (strcmp(nombre,comparacion)= 0){
+			
+			printf ("\n\nartista: %s titulo: %s duracion: %d segundos \n tamaño: %d KB \n formato mp3",tema[i].artista,tema[i].titulo,tema[i].duracion, tema[i].tamanioKb );	
+			
+		}
+		
+		comparacion=tema[i].titulo;
+		
+		if (strcmp(nombre,comparacion)==0){
+			
+			printf ("\n\nartista: %s titulo: %s duracion: %d segundos \n tamaño: %d KB \n formato mp3",tema[i].artista,tema[i].titulo,tema[i].duracion, tema[i].tamanioKb );	
+			
+		}
+		
+	}
+}
+
+
+void menu(){
+	cancion tema[100];
+	int opcion;
+	cancion vector[100];
+	int longitud;
+	
+	opcion=0;
+	
+	while (opcion!=5){
+		
+		printf ("\n\n        MENU        \n");
+		printf ("\nelija un numero segun la opcion requerida\n");
+		printf ("\n1. ingresar cancion\n");
+		printf ("2. mostrar cancion.\n");
+		printf ("3. mostrar titulo de todas las canciones\n");
+		printf ("4. buscar cancion\n");
+		printf ("0. salir\n\n");
+		printf ("ingrese la opcion:  ");
+		scanf ("%d", &opcion);
+		
+	    
+		switch (opcion){
+		
+		
+		case 1:  
+			ingresaDatos (tema,longitud=0);
+		break;
+		case 2: 
+			mostrarDatos (tema, longitud);	
+		break;
+		case 3:  
+			muestraTitulos (tema, longitud);
+		break;
+		case 4: 
+			buscarCancion (vector, longitud);  
+		break;
+		case 0:
+			exit(0);
+			break;
+		default: 
+			printf ("opcion no valida."); 
+		break;
+		}    
+	}
+}
+
+
+
---
+++ TALLER_4/taller_4_h.cpp
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct proveedores{
+	
+	char nombre[30];
+	int cantidadVendida;
+	float precioUnitario;
+	float importe;
+	
+};
+
+void menu ();
+void registrarProveedores (proveedores vector[],int &longitud);
+void imprimirProveedores(proveedores vector[], int longitud);
+
+int main() {
+
+	menu ();
+	
+	return 0;
+}
+
+void registrarProveedores (proveedores vector[],int &longitud){
+	
+	int i;
+	
+	
+	printf ("ingrese la cantidad de vendedores: ");
+	scanf("%d",&longitud);
+	
+	for (i=0; i<longitud; i++){
+		printf ("ingrese el nombre del vendedor: ");
+		fflush(stdin);
+		fgets (vector[i].nombre,30,stdin);
+		
+		printf ("ingrese la cantidad vendida: ");
+		scanf ("%d",&vector[i].cantidadVendida);
+		
+		printf ("ingrese el precio unitario: ");
+		scanf ("%f",&vector[i].precioUnitario);
+		
+		vector[i].importe=vector[i].cantidadVendida*vector[i].precioUnitario;
+		
+		longitud+=1;
+	}	
+}
+
+void imprimirProveedores(proveedores vector[], int longitud){
+	
+	int i;
+	int mayor=0;
+	int menor=999999;
+	
+	for (i=0; i<longitud; i++){
+		printf ("el vendedor es: \n");
+		printf ("vendedor: %s \t cantidad: %d \t precio: %f \t importe: %f \n", vector[i].nombre, vector[i].cantidadVendida, vector[i].precioUnitario, vector[i].importe);
+		
+		if (vector[mayor].importe<vector[i].importe){
+			
+			mayor=i;
+		}
+		printf ("proveedor menor: %s \n", vector[menor].nombre);
+		printf ("proveedor mayor: %s \n", vector[mayor].nombre);
+		
+	}
+
+}
+
+void menu (){
+	int longitud=0;
+	int opcion;
+	proveedores vector[100];
+	opcion=0;
+	while (opcion!=3){
+		
+		printf ("\n    MENU    \n");
+		printf ("1. ingresar proveedores \n");
+		printf ("2. mostrar proveedores \n");
+		printf ("0. salir \n");
+		printf ("ingrese opcion:  ");
+		scanf ("%d", &opcion);
+		switch (opcion){
+			
+		case 1: 
+			registrarProveedores (vector, longitud);
+			break;
+		case 2: 
+			imprimirProveedores(vector, longitud);
+			break;
+		case 0:
+			exit(0);
+			break;
+		default: 
+			printf ("opcion no valida."); 
+			break;
+		}
+	}
+}
+
+
+	
